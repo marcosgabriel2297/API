5c23866c5f04ac5627881bfbeb16a4d40ab9cf1e
Marcos Gabriel Vera <mvera@moodtechnology.com.ar>
2023-08-06 19:20:56 -0300

    feat: create authenticateEmail user service

--- log-in/src/services/userService.ts
+++ log-in/src/services/userService.ts
@@ -1,9 +1,31 @@
-import User, { UserInterface } from '../models/User';
+import ERROR_CODES from '../constants/errorCodes';
+import User, { IUser, UserInterface } from '../models/User';
+import { comparePassword } from '../utils/bycriptUtil';
+import { signJwt } from '../utils/jwtUtil';
+
+const { USER_NOT_EXISTS, PASSWORD_NOT_VALID } = ERROR_CODES;
 
 const findByEmail = (email: string) => User.findOne({ email });
 
 const create = (user: UserInterface) => User.create(user);
 
-const userService = { create, findByEmail };
+const signUserWithJwt = (user: IUser) => {
+    const token = signJwt(user);
+    return { ...user.toJSON(), ...{ token } };
+};
+
+const authenticateEmail = async (credentials: UserInterface) => {
+    const user = await findByEmail(credentials.email);
+    if (!user) return Promise.reject(USER_NOT_EXISTS);
+
+    const isPasswordValid = await comparePassword(credentials.password, user.password);
+    if (!isPasswordValid) {
+      return Promise.reject(PASSWORD_NOT_VALID);
+    }
+
+    return signUserWithJwt(user);
+  };
+
+const userService = { create, findByEmail, authenticateEmail };
 
 export default userService;
