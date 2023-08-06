9b742e2b59075162c83b1baf60cd9031e62e7917
Marcos Gabriel Vera <mvera@moodtechnology.com.ar>
2023-08-06 17:52:36 -0300

    fix: add limit and extended to server

--- log-in/src/server.ts
+++ log-in/src/server.ts
@@ -5,7 +5,7 @@ import apiRouter from './router';
 const server = express();
 
 server.use(express.json({}));
-server.use(express.urlencoded({}));
+server.use(express.urlencoded({ limit: '50mb', extended: false }));
 
 server.use('/api', apiRouter);
 
